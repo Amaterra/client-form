43b3b3c0c57cc1daf5addfbd20b8e4abd7914c41
Amaterra <ami.less@yandex.ru>
2024-03-05 13:55:08 +0200

    edit file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/client-form/" : "/",
+  publicPath: process.env.NODE_ENV === "development" ? "/client-form/" : "/",
   transpileDependencies: true
 })
